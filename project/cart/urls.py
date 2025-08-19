fe688c867053efbb0d66e898cfad3306c040c4f6
affilpm <affilpm2004@gmail.com>
2025-08-19 18:17:00 +0530

    chore: replaced _ with - for urls

--- project/cart/urls.py
+++ project/cart/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
   
     path('cart/', views.cart, name='cart'),
-    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
-    path('remove_from_cart', views.remove_from_cart, name='remove_from_cart'),
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart', views.remove_from_cart, name='remove_from_cart'),
   
 ]
